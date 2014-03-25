3fcc5f0cfabf4b9a9d53395d678e1f271bfbc881
codeinjector <nalin.chhibber@gmail.com>
2014-03-25 21:59:23 +0530

    update

--- run.sh
+++ run.sh
@@ -2,13 +2,16 @@ if [ "$1" == "" ];
 then
      ./bin/main
 
-elif [ "$1" == "-train" ];
+elif [ "$1" == "-GUI" ];
 then
      echo "not functional atm" #./bin/cs
 
-elif [ "$1" == "-init" ];#; -a "$2" == "rossler" ];
+elif [ "$1" == "-f" ];
 then 
      echo "not functional atm" #./bin/ca
+
+elif [ "$1" == "-t" ];
+     ./bin/main -t
     
 elif [ "$1" == "-help" ];
 then 
@@ -18,8 +21,8 @@ then
 	echo -e "\t ./run.sh\t\t\t\t:Enables CLI(default) Mode"	
         echo -e "\t ./run.sh -GUI\t\t\t\t:Enables GUI Mode(currently non-operational)"
 	echo -e "\t ./run.sh -help\t\t\t\t:Displays usage/help section"
-	#echo -e "\t ./run.sh -train\t\t\t:Forks the module that generates training data"
-	#echo -e "\t ./run.sh -init \t\t\t:Initialise the cryptanalysis module"
+	echo -e "\t ./run.sh -t\t\t\t\t:Generate training data"
+	echo -e "\t ./run.sh -f <text> <filename>\t\t:Perform Frequency Analysis"
 
 else
   echo "err..invalid usage"

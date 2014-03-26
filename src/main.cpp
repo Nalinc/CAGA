c6d4d21fcf637d443c4ec70f3820d31a26434724
codeinjector <nalin.chhibber@gmail.com>
2014-03-26 16:46:17 +0530

    update

--- src/main.cpp
+++ src/main.cpp
@@ -39,6 +39,7 @@ int main(int argc,char **argv)
    cout<<"\n==========Initializing Genetic Algorithms===========";
 	GA ga;
 //	cout<<pt;
+   cout<<"\n========Encrypting Data Using Chromosome Pool=======";
 	cmap cm(pt);
 	
 

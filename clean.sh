c67f10195304c9c99465ef7bdcaa3ebbf3e4abe7
codeinjector <nalin.chhibber@gmail.com>
2014-05-06 05:52:00 +0530

    update

--- clean.sh
+++ clean.sh
@@ -1,47 +1,51 @@
 
 if [ ! -f ./data/* ]; 
 then 
-  echo "Directory Already Empty"; 
+  echo "Removing Files"; 
 else
   rm ./data/*
 fi
 
 if [ ! -f ./chromosomes/*.dat ]; 
 then 
-  echo "Directory Already Empty";
+  echo "Removing Files";
 else
   rm ./chromosomes/*.dat
 fi
 
 if [ ! -f ./chromosomes/.*.dat ]; 
 then 
-  echo "Directory Already Empty";
+  echo "Removing Files";
 else
   rm ./chromosomes/.*.dat
 fi
 
 if [ ! -f ./frequencyanalysis/uni/* ]; 
-then echo "Directory Already Empty";
+then echo "Removing Files";
 else
   rm ./frequencyanalysis/uni/*
 fi
 
 if [ ! -f  ./frequencyanalysis/bi/* ]; 
 then 
-  echo "Directory Already Empty";
+  echo "Removing Files";
 else
   rm ./frequencyanalysis/bi/*
 fi
 
 if [ ! -f   ./frequencyanalysis/tri/* ]; 
 then 
-  echo "Directory Already Empty";
+  echo "Removing Files";
 else
-rm ./frequencyanalysis/tri/*
+  rm ./frequencyanalysis/tri/*
 fi
 
-
-
+if [ ! -f ./frequencyanalysis/*.png ];
+then
+   echo "Removing Files"
+else
+  rm ./frequencyanalysis/*.png
+fi
 
 
 
